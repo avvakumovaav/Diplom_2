8b47af4b4929760089c9b569dd4873f330439216
avvakumovaav <nikulina.anastasiya.v@yandex.ru>
2024-11-03 20:59:12 +0300

    feat: api test for order

--- src/main/java/praktikum/order/OrderChecks.java
+++ src/main/java/praktikum/order/OrderChecks.java
@@ -24,15 +24,34 @@ public class OrderChecks {
 
     @Step("Проверка неуспешного создания заказа с неверным хешем ингредиентов")
     public void checkCreatedWithInvalidIngredientHash(ValidatableResponse response) {
-        response.assertThat().statusCode(500)
+        response.assertThat()
+                .statusCode(500)
                 .and()
                 .body(containsString("Internal Server Error"));
     }
 
     @Step("Проверка успешного создания заказа c ингредиентами без авторизации")
     public void checkCreatedWithIngredientsWithoutAuthUser(ValidatableResponse response) {
-        response.assertThat().statusCode(HTTP_OK)
+        response.assertThat()
+                .statusCode(HTTP_OK)
                 .and()
                 .body("success", equalTo(true));
+    }
+
+    @Step("Проверка успешного получения заказа авторизанного пользователя")
+    public void checkGetOrderWithAuthUser(ValidatableResponse response) {
+        response.assertThat()
+                .statusCode(HTTP_OK)
+                .and().body("success", equalTo(true))
+                .and().body("orders", notNullValue())
+                .and().body("total", notNullValue())
+                .and().body("totalToday", notNullValue());
+    }
+
+    @Step("Проверка неуспешного получения заказа неавторизанного пользователя")
+    public void checkGetOrderWithoutAuthUser(ValidatableResponse response) {
+        response.assertThat().statusCode(HTTP_UNAUTHORIZED)
+                .and().body("success", equalTo(false))
+                .and().body("message", equalTo("You should be authorised"));
     }
 }
